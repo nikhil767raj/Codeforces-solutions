1970e16788eaf292e7b728ccdd5a352ad15e7617
Nikhil Raj <68438890+nikhil767raj@users.noreply.github.com>
2021-01-25 20:02:07 +0530

    Add files via upload

--- CF-1352B.cpp
+++ CF-1352B.cpp
@@ -1,30 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-	int t; cin >> t;
-	while(t--){
-		int n,k,z; cin >> n >> k;
-		if(n == k){
-			while(k--){
-				cout << "1" << " ";
-			}
-			cout << endl;
-		}
-		else{
-			if(n%k == 0){
-				z=n/k;
-				while(z--){
-					cout << k << " ";
-				}
-				cout << endl;
-			}
-			else{
-				z=n/k;
-				while(z--){
-					
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        int n, k, x, xx; cin >> n >> k;
+        if (n == k) {
+            cout << "YES" << endl;
+            for (int i = 0; i < n; i++) cout << "1" << " ";
+            cout << endl;
+        }
+        else if (n < k) {
+            cout << "NO" << endl;
+        }
+        else {
+            if (n % 2 == 0) {
+                if (k > n / 2) {
+                    x = k - 1;
+                    xx = n - x;
+                    if (xx % 2 == 0) cout << "NO" << endl;
+                    else {
+                        cout << "YES" << endl;
+                        for (int i = 1; i <= k - 1; i++) cout << "1" << " ";
+                        cout << xx << endl;
+                    }
 
-				}
-			}
-		}
-	}
+                }
+                else if (k == n / 2) {
+                    cout << "YES" << endl;
+                    for (int i = 1; i <= k; i++) cout << "2" << " ";
+                    cout << endl;
+                }
+                else if (k < n / 2) {
+                    cout << "YES" << endl;
+                    for (int i = 1; i <= k - 1; i++) cout << "2" << " ";
+                    int z = 2 * (k - 1);
+                    int zz = n - z;
+                    cout << zz << endl;
+                }
+            }
+            else {
+                if (k % 2 == 0) {cout << "NO" << endl;}
+                else {
+                    x = k - 1;
+                    xx = n - x;
+                    if (xx % 2 == 0) cout << "NO" << endl;
+                    else {
+                        cout << "YES" << endl;
+                        for (int i = 1; i <= k - 1; i++) cout << "1" << " ";
+                        cout << xx << endl;
+                    }
+                }
+            }
+        }
+    }
 }
